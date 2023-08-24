21d563c5846dc0eafe4460bb13de0fe03d2705b0
Rayne Mallari <raynemllr.5@gmail.com>
2023-08-24 12:48:40 +0800

    Add styling

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 function Topbar() {
   return (
@@ -28,6 +29,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: 'py-2 px4',
             },
