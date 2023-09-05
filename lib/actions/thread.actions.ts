468a653567bb1f5198704e374ec4352a8f712099
Rayne Mallari <raynemllr.5@gmail.com>
2023-09-05 13:17:33 +0800

    Add addCommentToThread server action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -107,3 +107,41 @@ export async function fetchThreadById(id: string) {
     throw new Error(`Error fetching thread: ${error.message}`);
   }
 }
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+    // Find the original thread by its ID
+    const orignialThread = await Thread.findById(threadId);
+
+    if (!orignialThread) {
+      throw new Error('Thread not found');
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    // Save the new thread
+    const savedCommentThread = await commentThread.save();
+
+    // Update the original thread to include the new comment
+    orignialThread.children.push(savedCommentThread._id);
+
+    // Save the original thread
+    await orignialThread.save();
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`);
+  }
+}
